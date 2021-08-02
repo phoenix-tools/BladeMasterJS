4d6e003774ef637a408125a05b36b3cc935c4791
GITUSER <hudson@inboundnow.com>
2021-08-01 20:14:22 -0500

    Preparing API

--- bsscanAPI/bsscanAPI.js
+++ bsscanAPI/bsscanAPI.js
@@ -5,7 +5,7 @@ bscscan.setApiKey('APWFP16UP19G7CCDQ39788NNI5UAJJIW3W')
 
 const start = async () => {
     try {
-        const balance = await account.getBnbBalance('0x765090aB712984081aeE059eA7025C48a4198183')
+        const balance = await account.getBnbBalance('0x2c238ad3411BdcDC0E4175f9320924ebC335E9E0')
 
         console.log(`Your balance is: ${balance}`)
     } catch (err) {
